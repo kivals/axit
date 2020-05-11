215281ff1b2e0dce059246b47772d842a81e42f8
melnik <kivals.90@yandex.ru>
2020-05-11 21:24:01 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const clean = require('./gulp/tasks/clean');
 const scss = require('./gulp/tasks/scss');
 const script = require('./gulp/tasks/script');
 const fonts = require('./gulp/tasks/fonts.js');
+const images = require('./gulp/tasks/image.js');
 
-const build = gulp.parallel(pug2html, scss, script, fonts);
+
+const build = gulp.parallel(pug2html, scss, script, fonts, images);
 exports.default = gulp.series(clean, build, serve);
