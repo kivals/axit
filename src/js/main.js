a433870f4c771705f313f339a6d6d2643c0c6489
melnik <melnik@bifit.com>
2020-05-15 17:46:39 +0300

    js slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const burgerIcon = document.querySelector('.burger-icon');
 const burgerMenu = document.querySelector('.burger-menu');
+const tabSelector = document.querySelector('.tab__selector');
 
 if (burgerIcon) {
   burgerIcon.addEventListener('click', () => {
@@ -27,8 +28,27 @@ function adaptive_header(width) {
   }
 }
 
+function highlightSelector(newSelector) {
+  const activeSelector = tabSelector.getElementsByClassName('active');
+  if (activeSelector) {
+    activeSelector.classList.remove('active');
+    newSelector.classList.add('active');
+  }
+}
+
 window.addEventListener('resize', () => {
   adaptive_header(window.outerWidth);
+});
+
+tabSelector.addEventListener('click', (e) => {
+  const selector = e.target.closest('.selector__item');
+  if (!selector) {
+    return;
+  }
+  highlightSelector(selector);
+  selector.classList.toggle('active');
+  const sliderName = selector.dataset.slider;
+  console.log(sliderName);
 });
 
 adaptive_header(window.outerWidth);
